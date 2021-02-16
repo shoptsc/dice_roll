f2abdbdb8f23beba44baca01af6dd7b20203617c
shoptsc <sanusihamed@gmail.com>
2021-02-16 23:09:48 +0100

    new commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const text = document.querySelector('h1');
+const img1 = document.querySelector('.img1');
+const img2 = document.querySelector('.img2');
+
+let number1 = Math.floor(Math.random() * 6) + 1;
+let number2 = Math.floor(Math.random() * 6) + 1;
+
+let image1 = `images/dice${number1}.png`
+let image2 = `images/dice${number2}.png`
+
+img1.setAttribute('src', image1)
+img2.setAttribute('src', image2)
+
+if (number1 === number2) {
+    text.innerHTML = 'Draw!'
+} else if (number1 > number2) {
+    text.innerHTML = '🚩Player 1 Wins!'
+} else {
+    text.innerHTML = 'Player 2 Wins!🚩'
+}
